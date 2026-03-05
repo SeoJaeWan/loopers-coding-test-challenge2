64fd4b6291f913ec5533ac6ac58db81a26d389d4
SeoJaeWan <sjw7324@naver.com>
2026-03-05 20:08:28 +0900

    정규표현식을 잘쓰자.

--- sjw7324/16205.js
+++ sjw7324/16205.js
@@ -10,7 +10,29 @@ readline
     inputs.push(line.trim());
   })
   .on("close", function () {
-    const [currentType, str] = inputs[0].split(" ");
+    const [typeStr, str] = inputs[0].split(" ");
+    const type = Number(typeStr);
 
-    console.log(str.split(/[A-Z|\_]/));
+    const toWords = (type, s) => {
+      if (type === 2) {
+        return s.split("_");
+      }
+      return s
+        .replace(/([A-Z])/g, " $1")
+        .trim()
+        .split(" ")
+        .map((w) => w.toLowerCase());
+    };
+
+    const capitalize = (w) => w[0].toUpperCase() + w.slice(1);
+
+    const words = toWords(type, str);
+
+    const camel = words[0] + words.slice(1).map(capitalize).join("");
+    const snake = words.join("_");
+    const pascal = words.map(capitalize).join("");
+
+    console.log(camel);
+    console.log(snake);
+    console.log(pascal);
   });
